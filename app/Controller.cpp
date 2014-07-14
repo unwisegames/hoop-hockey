54d5a9320dc48656e429878b258a28a62c22c887
Marcelo Cantos <marcelo.cantos@gmail.com>
2014-07-14 21:43:00 +1000

    Latest bricabrac

--- app/Controller.cpp
+++ app/Controller.cpp
@@ -80,7 +80,7 @@ void Controller::onDraw() {
     SpriteProgram::draw(atlas.hoop[m->game->hoop_state()], pmv() * mat4::translate({0, 5.3, 0}));
 
     // This currently does nothing because a new game starts immediately after a game ends.
-    if (!*m->game) {
+    if (m->game->hasEnded()) {
         SpriteProgram::drawText(std::to_string(m->game->score()), scorefont.glyphs, TextAlign::right,
                                 pmv() * mat4::translate({0, 0, 0}));
     }
